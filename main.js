68094ead9fd406d56381b877ba68eed4f8455e9a
mikeBRYAN <157809620+mike19bryan4@users.noreply.github.com>
2024-06-06 21:40:39 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+// nav icon
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+// sroll section
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+
+        };
+    });
+    // sticky navbar
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky',window.scroll > 100);
+
+    // remove navbar-icon
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+window.addEventListener('scroll', handleScroll);
+
+function handleScroll() {
+    var reveals = document.querySelectorAll('.reveal');
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+
+        if (revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        };
+    };
+};
